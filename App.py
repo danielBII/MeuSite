deebbf8dbe9b48557a8e4e8b6aa8596ab5166d71
MiriSilva <mirisb14@gmail.com>
2019-11-21 16:55:37 -0300

    Criação do python

---
+++ App.py
@@ -0,0 +1,39 @@
+from flask import Flask, render_template
+from flask_sqlalchemy import SQLAlchemy
+
+from formulario import MensagemForm
+
+app = Flask(__name__)
+db = SQLAlchemy(app)
+app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///minhasmensagens.db'
+app.config['SECRET_KEY'] = 'dfsshfehryuakbfpefeg875368jgkdfoadydgkfbdfnlshobcl'
+
+class Mensagem(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    mensagem = db.Column(db.Text, nullable=False)
+
+    def __repre__(self):
+        return '<<<MSG: %r>>>' % self.mensagem
+
+
+@app.route('/')
+def index():
+
+    msg = Mensagem.query.all
+
+    return render_template('index.html')
+
+@app.route('/nova-mensagem', methods=['POST', 'GET'])
+def novaMensagem():
+
+    form = MensagemForm()
+    if form.validate_on_submit():
+        msg = Mensagem()
+        msg.mensagem = form.mensagem.data
+        db.session.add(msg)
+        db.session.commit()
+
+    return render_template('mensagens.html', form=form)
+
+if __name__== "__main__":
+    app.run(debug=True) 
